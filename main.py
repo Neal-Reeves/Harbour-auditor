d20394c1d0178ff210c4df8110ad3aa6a58eb9aa
Neal Reeves <n.reeves@ucl.ac.uk>
2026-07-16 15:37:07 +0100

    Initialised dataclasses for portal, tracker and audit users

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import pandas as pd
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
+from typing import Optional
+from datetime import date
+
+@dataclass
+class PortalUser:
+    name: str
+    email: str
+    active_workspaces: int = 0
+    controlled_tier_access: bool = False
+
+@dataclass
+class TrackerUser:
+    name: str
+    email: str
+    project_end_date: Optional[date] = None
+    supervisor_name: str
+    supervisor_email: str
+
+@dataclass
+class AuditOutput:
+    name: str
+    email: str
+    status: str = "Active" #Options: Active, Left UCL, Project Expired, Ineligible
+    supervisor_name: Optional[str] = None
+    supervisor_email: Optional[str] = None
 
 def open_tracker(tracker_url):
     return pd.read_excel(tracker_url)
@@ -7,7 +33,7 @@ def open_tracker(tracker_url):
 def all_of_us_parser(html_file_path):
     #Parses All of Us HTML report to extract active users
     with open(html_file_path, "r", encoding="utf-8") as logfile:
-        soup = BeautifulSoup(f.read(), "html.parser")
+        soup = BeautifulSoup(logfile.read(), "html.parser")
     
     users = []
 
